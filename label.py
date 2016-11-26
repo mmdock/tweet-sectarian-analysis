21d3a8336513f46269a1a7747b0ff686c3166efa
Dixon Styres <dstyres@go.olemiss.edu>
2016-11-25 23:13:26 -0600

    Simple utility to label tweets see tweets.csv for input specifics

---
+++ label.py
@@ -0,0 +1,34 @@
+##Labler, reads in a csv of tweet texts displays so that we can manully label text.
+##Outputs labled tweets to a csv that we can combine later and feed to a model.
+
+##DCS
+
+import pandas as pd
+import sys
+
+def label(tweets):
+    ratings = []
+    data = pd.read_csv(tweets)
+    for index, row in data.iterrows():
+        print(row['text'])
+        while True:
+            try:
+                rating = str(input("Select Sentiment 5=Pro Racism, 1=Against Racism: "))
+            except ValueError:
+                print("Sorry, I didn't understand that.")
+                continue
+
+            if  (rating == "5"):
+                ratings.append('5')
+                break
+            elif(rating == "1"):
+                ratings.append('1')
+                break
+            else:
+                print("Invalid Entry")
+                print(row['text'])
+                continue
+    data['sentiment'] = ratings
+    data.to_csv('out.csv')
+
+label("tweets.csv")
