5aaeb77a2c58e6f1685967cb5b5aabc4118b5c33
Morgan Dock <morgan.dock@gmail.com>
2016-11-27 15:46:22 -0600

    What a doozey

--- sent.py
+++ sent.py
@@ -2,23 +2,55 @@ import json
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-from textblob import TextBlob
-from elasticsearch import Elasticsearch
-import time
-import io
-# import twitter keys and tokens
+from flask import Flask, render_template
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map, icons
+import threading
 from config import *
 
-# create instance of elasticsearch
-es = Elasticsearch()
+app = Flask(__name__, template_folder="web_serve/mytemplate")
+app.config['GOOGLEMAPS_KEY'] = "AIzaSyAZzeHhs-8JZ7i18MjFuM35dJHq70n3Hx4"
+GoogleMaps(app, key="AIzaSyAZzeHhs-8JZ7i18MjFuM35dJHq70n3Hx4")
 
+icons = ['//maps.google.com/mapfiles/ms/icons/blue-dot.png', '//maps.google.com/mapfiles/ms/icons/green-dot.png', icons.dots.yellow, icons.dots.red]
+class Markers(object):
+    class __Markers:
+        def __init__(self):
+            self.val = [None]
+        def __str__(self):
+            return ('self' + '\n'.join(self.val))
+    instance = None
+    def __new__(cls): # __new__ always a classmethod
+        if not Markers.instance:
+            Markers.instance = Markers.__Markers()
+        return Markers.instance
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+    def __setattr__(self, name):
+        return setattr(self.instance, name)
+
+@app.route("/")
+def fullmap():
+    m = Markers()
+    fullmap = Map(
+        identifier="fullmap",
+        varname="fullmap",
+        style=(
+            "height:100%;"
+            "width:100%;"
+            "top:0;"
+            "left:0;"
+            "position:absolute;"
+            "z-index:200;"
+        ),
+        lat=37.4419,
+        lng=-122.1419,
+        markers= m.val,
+        zoom = "4",
+    )
+    return render_template('example_fullmap.html', fullmap=fullmap)
 
 class TweetStreamListener(StreamListener):
-
-    def __init__(self, start_time, time_limit=60):
-        self.time = start_time
-        self.limit = time_limit
-        self.tweet_data = []
         
     # on success
     def on_data(self, data):
@@ -30,42 +62,20 @@ class TweetStreamListener(StreamListener):
             return
         if not dict_data.get("place"):
             return
-#        tweet = TextBlob(dict_data["text"])
+        m = Markers()
         print(json.dumps(dict_data["place"], indent=4, sort_keys=True))
-        # determine if sentiment is positive, negative, or neutral
-##        if tweet.sentiment.polarity < 0:
-##            sentiment = "negative"
-##        elif tweet.sentiment.polarity == 0:
-##            sentiment = "neutral"
-##        else:
-##            sentiment = "positive"
-
-        # output sentiment
-        #print (str(tweet.sentiment.polarity) + " " + sentiment)
-
-        # add text and sentiment info to elasticsearch
-##        es.index(index="sentiment",
-##                 doc_type="test-type",
-##                 body={"author": dict_data["user"]["screen_name"],
-##                       "date": dict_data["created_at"],
-##                       "location": dict_data["user"]["location"],
-##                       "message": dict_data["text"],
-##                       "polarity": tweet.sentiment.polarity,
-##                       "subjectivity": tweet.sentiment.subjectivity,
-##                       "sentiment": sentiment,
-##                       "timestamp": dict_data["timestamp_ms"],
-##                       "place": dict_data["place"]["full_name"]}
-##                 )
+        m.val.append({'icon': icons[0], 'lat': dict_data["place"]["bounding_box"]["coordinates"][0][0][0], 'lng': dict_data["place"]["bounding_box"]["coordinates"][0][0][1], 'infobox': dict_data["text"]})
+        print(m.val)
         return True
 
     # on failure
     def on_error(self, status):
         print (status)
 
-if __name__ == '__main__':
+def begin_stream():
 
     # create instance of the tweepy tweet stream listener
-    listener = TweetStreamListener(time.time(), time_limit=60)
+    listener = TweetStreamListener()
 
     # set twitter keys/tokens
     auth = OAuthHandler(consumer_key, consumer_secret)
@@ -76,5 +86,12 @@ if __name__ == '__main__':
     # create instance of the tweepy stream
     stream = Stream(auth, listener)
 
-    # search twitter for "congress" keyword
     stream.filter(track=['Trump'], languages = ['en'])
+    
+if __name__ == '__main__':
+
+
+    # search twitter for "congress" keyword
+    threading.Thread(target=begin_stream).start()
+    app.run(debug=True, use_reloader=True)
+
