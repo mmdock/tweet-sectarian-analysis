043375a0b78c584c11192b7b16bce805c3f2f8e4
Morgan Dock <morgan.dock@gmail.com>
2016-11-28 16:37:53 -0600

    Added some comments and restructuring to sent.py

--- sent.py
+++ sent.py
@@ -219,73 +219,6 @@ def state_statistics(state, k):
 
     return render_template('city_statistics.html', values=values, labels=labels)
 
-class TweetStreamListener(StreamListener):
-    
-    def typefind(val):
-        if(val == 1):
-            return("Religion")
-        elif(val == 2):
-            return("Gender")
-        elif(val == 3):
-            return("Racial")
-        elif(val == 4):
-            return("Sexual Orientation")
-        elif(val == 5):
-            return("Disability")
-        elif(val == 6):
-            return("Political")
-        elif(val == 7):
-            return("Other")
-
-    def __init__(self):
-        self.classifier = KNNClassifier('train/train.csv')
-
-    # on success
-    def on_data(self, data):
-        # decode json
-        dict_data = json.loads(data)
-        if "text" not in dict_data:
-            return
-        if not dict_data.get("place"):
-            return
-        print(json.dumps(dict_data, indent=4, sort_keys=True))
-        m = Markers()
-
-        if dict_data["place"]["full_name"]:
-            c1 = dict_data["place"]["full_name"]
-            c1 = c1.strip().split(", ")
-        else:
-            c1 = ["None","None"]
-
-        author = [dict_data["user"]["screen_name"]]
-        city = [c1[0]]
-        try:
-            state = [c1[1]]
-        except IndexError:
-            state = 'None'
-        lng = [(dict_data["place"]["bounding_box"]["coordinates"][0][0][0] + dict_data["place"]["bounding_box"]["coordinates"][0][1][0] + dict_data["place"]["bounding_box"]["coordinates"][0][2][0] + dict_data["place"]["bounding_box"]["coordinates"][0][3][0])/4]
-        lat = [(dict_data["place"]["bounding_box"]["coordinates"][0][0][1] + dict_data["place"]["bounding_box"]["coordinates"][0][1][1] + dict_data["place"]["bounding_box"]["coordinates"][0][2][1] + dict_data["place"]["bounding_box"]["coordinates"][0][3][1])/4]
-        text = [(dict_data["text"])]
-
-        if(dict_data["place"]["country_code"] == "US"):
-            sentiment = self.classifier.classify(dict_data['text'], KNNClassifier.SENTIMENT)
-            category = self.classifier.classify(dict_data['text'], KNNClassifier.CATEGORY)
-
-            m.val.append({'icon': icons[sentiment], 'lng': lng, 'lat': lat, 'infobox':  typefind(category) + ": " + text[0]  })
-            d = {'author': author, 'city': city, 'state':state, 'lat':lat, 'lng': lng, 'text': text, 'sentiment': sentiment, 'category': category}
-            df = pd.DataFrame(data=d, columns=["author", "city", "state", "lat", "lng", "text", "sentiment", "category"])
-            with open('stats.csv', 'a') as f:
-                if not path.exists("stats.csv"):
-                    df.to_csv(f, header = True)
-                else:
-                    df.to_csv(f, header = False)
-
-        return True
-
-    # on failure
-    def on_error(self, status):
-        print (status)
-
 def typefind(val):
     if(val == 1):
         return("Religion")
@@ -301,6 +234,67 @@ def typefind(val):
         return("Political")
     elif(val == 7):
         return("Other")
+    
+class TweetStreamListener(StreamListener):
+
+    def __init__(self):
+        self.classifier = KNNClassifier('train/train.csv')
+
+    # on success
+    def on_data(self, data):
+        # decode json
+        dict_data = json.loads(data)
+
+        #Must have a place object, must have a text object
+        if "text" not in dict_data:
+            return
+        if not dict_data.get("place"):
+            return
+        #print(json.dumps(dict_data, indent=4, sort_keys=True))
+
+        m = Markers() #Get the Markers instance
+
+        #Use full_name if exists, otherwise, set None, None - May need changing
+        if dict_data["place"]["full_name"]:
+            c1 = dict_data["place"]["full_name"]
+            c1 = c1.strip().split(", ")
+        else:
+            c1 = ["None","None"]
+        #User info
+        author = [dict_data["user"]["screen_name"]]
+
+        #City from full_name
+        city = [c1[0]]
+        try:
+            #Due to how full_name is weirdly set up, sometimes a place can be Region, Country rather than City, Region, may need to handle that as well
+            state = [c1[1]]
+        except IndexError:
+            state = 'None'
+
+        # Use the average of the lat/lngs of the bounding box for the marker coordinates.
+        lng = [(dict_data["place"]["bounding_box"]["coordinates"][0][0][0] + dict_data["place"]["bounding_box"]["coordinates"][0][1][0] + dict_data["place"]["bounding_box"]["coordinates"][0][2][0] + dict_data["place"]["bounding_box"]["coordinates"][0][3][0])/4]
+        lat = [(dict_data["place"]["bounding_box"]["coordinates"][0][0][1] + dict_data["place"]["bounding_box"]["coordinates"][0][1][1] + dict_data["place"]["bounding_box"]["coordinates"][0][2][1] + dict_data["place"]["bounding_box"]["coordinates"][0][3][1])/4]
+        text = [(dict_data["text"])]
+        
+        #Only US tweets
+        if(dict_data["place"]["country_code"] == "US"):
+            sentiment = self.classifier.classify(dict_data['text'], KNNClassifier.SENTIMENT)
+            category = self.classifier.classify(dict_data['text'], KNNClassifier.CATEGORY)
+            #Append to Makers list for display purposes
+            m.val.append({'icon': icons[sentiment], 'lng': lng, 'lat': lat, 'infobox':  typefind(category) + ": " + text[0]  })
+            d = {'author': author, 'city': city, 'state':state, 'lat':lat, 'lng': lng, 'text': text, 'sentiment': sentiment, 'category': category}
+            df = pd.DataFrame(data=d, columns=["author", "city", "state", "lat", "lng", "text", "sentiment", "category"])
+            with open('stats.csv', 'a') as f:
+                if not path.exists("stats.csv"):
+                    df.to_csv(f, header = True)
+                else:
+                    df.to_csv(f, header = False)
+
+        return True
+
+    # on failure
+    def on_error(self, status):
+        print (status)
 
 
 def begin_stream():
@@ -317,21 +311,23 @@ def begin_stream():
     # create instance of the tweepy stream
     stream = Stream(auth, listener)
 
+    #Apply filter on keywords
     stream.filter(track=keywords, languages = ['en'])
 
 if __name__ == '__main__':
 
-    m = Markers()
+    m = Markers() #Load Markers singleton class
+    
     old = pd.read_csv('stats.csv')
-    old = old.drop_duplicates()
+    old = old.drop_duplicates() # Handle duplication behavior
+    ## Load up previously pulled tweets into Marker Singleton
     for index, row in old.iterrows():
         lat = row[4]
         lng = row[5]
         text = row[6]
         sent = row[7]
-        type = row[8]
-        m.val.append({'icon': icons[sent], 'lng': lng, 'lat': lat, 'infobox': typefind(type) + ": " + text})
+        typ = row[8]
+        m.val.append({'icon': icons[sent], 'lng': lng, 'lat': lat, 'infobox': typefind(typ) + ": " + text})
 
-    # search twitter for "congress" keyword
     #threading.Thread(target=begin_stream).start()
     app.run(debug=True, use_reloader=True, host= '0.0.0.0')
